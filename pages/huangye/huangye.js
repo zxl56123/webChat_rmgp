7a440036985fedba0063e43fa2cbe8c47819214e
郑晓龙 <zxl56123@gmail.com>
2017-05-06 08:51:22 +0800

    微信定位（但是使用腾讯地图和百度地图反向编码回来的地址偏差还是比较大的，主要是街道这一级别偏差大）

--- pages/huangye/huangye.js
+++ pages/huangye/huangye.js
@@ -1,9 +1,17 @@
 // pages/huangye/huangye.js
 
-let BAIDU_AK_ZYB = "btsVVWf0TM1zUBEbzFz6QqWF&callback=renderReverse"
+let TENCENT_KEY = "AJPBZ-S6MRU-NFIVK-4BH5A-IZA57-OKB24"
 
 Page({
-  data: {},
+  data: {
+    nation: "",
+    province: "",
+    city: "",
+    district: "",
+    street: "",
+    street_number: ""
+
+  },
 
   getLocation: function () {
     var that = this
@@ -11,8 +19,8 @@ Page({
       type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标  
       success: function (res) {
         // success  
-        var longitude = res.longitude
-        var latitude = res.latitude
+        let longitude = res.longitude
+        let latitude = res.latitude
         that.loadCity(longitude, latitude)
       },
       fail: function () {
@@ -26,15 +34,31 @@ Page({
 
   loadCity: function (longitude, latitude) {
     var that = this
+
     wx.request({
-      url: 'https://api.map.baidu.com/geocoder/v2/?ak=' + BAIDU_AK_ZYB + '&location=' + latitude + ',' + longitude + '&output=json',
+
+      url: 'http://apis.map.qq.com/ws/geocoder/v1/?location=' + latitude + ',' + longitude + '&key=' + TENCENT_KEY,  
+      //url: 'https://api.map.baidu.com/geocoder/v2/?ak=0FuoX30MFf7YMrdS5Wi9GGAcHBblKDuu&callback=?&location=' + latitude + ',' + longitude + '&output=json',
+      
       data: {},
+      method: 'GET',
       header: {
         'Content-Type': 'application/json'
       },
       success: function (res) {
         // success  
         console.log(res);
+
+        that.setData({
+          nation: res.data.result.address_component.nation,
+          province: res.data.result.address_component.province,
+          city: res.data.result.address_component.city,
+          district: res.data.result.address_component.district,
+          street: res.data.result.address_component.street,
+          street_number: res.data.result.address_component.street_number
+        })
+
+        console.log(that.data.nation,that.data.province,that.data.city)
         // var city = res.data.result.addressComponent.city;
         // that.setData({ city: city });
       },
@@ -47,21 +71,21 @@ Page({
     })
   },
 
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-    //获取定位
-    this.getLocation()
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  }
+onLoad: function (options) {
+  // 页面初始化 options为页面跳转所带来的参数
+  //获取定位
+  this.getLocation()
+},
+onReady: function () {
+  // 页面渲染完成
+},
+onShow: function () {
+  // 页面显示
+},
+onHide: function () {
+  // 页面隐藏
+},
+onUnload: function () {
+  // 页面关闭
+}
 })
