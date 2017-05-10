1b3ef06953b9bd85de73a60f74b99ff81b98d20d
郑晓龙 <zxl56123@gmail.com>
2017-05-10 11:09:49 +0800

    便民-本地信息

--- pages/bianmin/bianmin.js
+++ pages/bianmin/bianmin.js
@@ -1,64 +1,114 @@
 // pages/bianmin/bianmin.js
 
 const config = require('../../config')
+var pageNo = 0;
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+    list: [],
+  },
+  /** 跳转要闻详情页面 */
+  tapHotNewsCell: function (event) {
+    wx.navigateTo({
+      url: '../index/HotNewsDetail/HotNewsDetail'
+    })
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
-  console.log("页面显示")
+    console.log("页面显示")
 
-  wx.showToast({
-  title: '成功123',
-  icon: 'success',
-  duration: 1500
-})
-
-console.log("--" + config.GET_HOT_NEWS + "---")
-
-  wx.request({
-    url: config.GET_HOT_NEWS,
-    data: {
-    "system": "02",
-    "tagId": "1",
-    "accessToken": "",
-    "scopeAddressCode": "",
-    "key": "",
-    "systemVersion": "10.3.1",
-    "imei": "A902EA47-B1B2-452A-96FB-4C7BCCBB149C",
-    "currentVersion": "3.1.6",
-    "sig": "",
-    "pageNo": "1",
-    "model": "iPhone 6s Plus (A1699)",
-    "pageSize": "20"
+    // wx.showToast({
+    //   title: '成功123',
+    //   icon: 'success',
+    //   duration: 1500
+    // })
   },
-    method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-    header: {
-      'content-type': 'application/json'
-      }, // 设置请求的 header
-    success: function(res){
-      // success
-      console.log(res.data)
 
-    },
-    fail: function(res) {
-      // fail
-    },
-    complete: function(res) {
-      // complete
-    }
-})
+  /** 下拉刷新 */
+  loadNewData: function () {
+    pageNo = 1;
+    this.requestData()
 
   },
-  onHide:function(){
-    // 页面隐藏
+
+  loadNewData_NextPage: function () {
+    pageNo += 1;
+    this.requestData();
   },
-  onUnload:function(){
-    // 页面关闭
+
+  requestData: function () {
+    var that = this
+
+    wx.request({
+      url: config.GET_HOT_NEWS + "?accessToken=",
+      data: {
+        "system": "02",
+        "tagId": "1",
+        "accessToken": "",
+        "scopeAddressCode": "5304",
+        "key": "",
+        "systemVersion": "10.1.1",
+        "imei": "C75C7019-29FA-4F2B-8311-BAA6F29D1845",
+        "currentVersion": "3.1.6",
+        "sig": "",
+        "pageNo": "1",
+        "model": "iPhone 5s (A1457\/A1518\/A1528\/A1530)",
+        "pageSize": "20"
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: { 'content-type': 'application/json' }, // 设置请求的 header
+      success: function (res) {
+        // success
+        if (pageNo == 1) {
+          that.setData({ list: res.data.data.list })
+
+        } else {
+          that.setData({ list: that.data.list.concat(res.data.data.list) })
+
+        }
+
+        console.log(that.data.list)
+
+      },
+      fail: function (res) {
+        // fail
+        pageNo--;
+      },
+      complete: function (res) {
+        // complete
+        wx.stopPullDownRefresh()
+      }
+    })
+
+  },
+  onLoad: function () {
+    console.log('onLoad')
+
+    this.loadNewData();
+
+    //调用应用实例的方法获取全局数据
+    // app.getUserInfo(function(userInfo){
+    //   //更新数据
+    //   that.setData({
+    //     userInfo:userInfo
+    //   })
+    // })
+  },
+  onPullDownRefresh: function () {
+    // 页面相关事件处理函数--监听用户下拉动作
+    this.loadNewData();
+
+  },
+  onReachBottom: function () {
+    // 页面上拉触底事件的处理函数
+    console.log("onReachBottom")
+
+    this.loadNewData_NextPage()
   }
+
 })
