3a8ea1670d7f500cffaac1c8adfd6d45509992b6
郑晓龙 <zxl56123@gmail.com>
2017-05-24 10:16:56 +0800

    下拉菜单 priceAndDistance: ['离我最近', '面额最高'], quancheng: ['1千米', '3千米', '5千米', '10千米', '全城']

--- pages/shenghuo/ValuePreference/ValuePreference.js
+++ pages/shenghuo/ValuePreference/ValuePreference.js
@@ -7,7 +7,21 @@ Page({
    */
   data: {
     currentTabIndex: 0,
-    CouponCategoryList: []
+    CouponCategoryList: [],
+
+    content: [],
+    priceAndDistance: [],
+    quancheng: [],
+    priceopen: false,
+    quanchengopen: false,
+    priceshow: false,
+    quanchengshow: false,
+    priceSelectedName: "面额最高",
+    quanchengSelectedName: "全城",
+    isfull: false,
+
+    shownavindex: ''
+
   },
   clickOrderTab: function (e) {
     //data = {};
@@ -30,6 +44,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      priceAndDistance: ['离我最近', '面额最高'],
+      quancheng: ['1千米', '3千米', '5千米', '10千米', '全城']
+    })
+
     this.requestCouponCategory("玉溪市")
   },
 
@@ -48,21 +67,23 @@ Page({
       dataType: '',
       success: function (res) {
         var dic = new Array()
+        var index = 0;
         for (var i = 0; i < res.data.data.length; i++) {
           let model = res.data.data[i];
           let array = model["nextCate"];
           for (var j = 0; j < array.length; j++) {
             let model_j = array[j];
             var temp = {};
-            temp["id"] = i + j;
+            temp["id"] = index;
             temp["count"] = model_j["count"];
             temp["cateName"] = model_j["cateName"];
             dic.push(temp);
+            index++;
           }
         }
 
         that.setData({ CouponCategoryList: dic })
-        console.log(that.data.CouponCategoryList)
+        //console.log(that.data.CouponCategoryList)
 
       },
       fail: function (res) { },
@@ -120,5 +141,86 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  
+  listprice: function (e) {
+    if (this.data.priceopen) {
+      this.setData({
+        priceopen: false,
+        quanchengopen: false,
+
+        priceshow: false,
+        quanchengshow: true,
+
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        content: this.data.priceAndDistance,
+        priceopen: true,
+        quanchengopen: false,
+
+        priceshow: false,
+        quanchengshow: true,
+
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+  },
+  listquancheng: function (e) {
+    if (this.data.quanchengopen) {
+      this.setData({
+        priceopen: false,
+        quanchengopen: false,
+
+        priceshow: true,
+        quanchengshow: false,
+
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        content: this.data.quancheng,
+        priceopen: false,
+        quanchengopen: true,
+
+        priceshow: true,
+        quanchengshow: false,
+
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+    console.log(e.target)
+  },
+  tapPriceCell: function (e) {
+    this.listprice("1")
+    let index = e.currentTarget.dataset.index
+    this.setData({ priceSelectedName: this.data.priceAndDistance[index] })
+    console.log(this.data.priceSelectedName)
+  },
+  tapQuanchengCell: function (e) {
+    this.listquancheng("2")
+    let index = e.currentTarget.dataset.index
+    this.setData({ quanchengSelectedName: this.data.quancheng[index] })
+    console.log(this.data.quanchengSelectedName)
+  },
+
+
+  hidebg: function (e) {
+
+    this.setData({
+      qyopen: false,
+      priceopen: false,
+      quanchengopen: false,
+      priceshow: true,
+      quanchengshow: true,
+      qyshow: true,
+      isfull: false,
+      shownavindex: 0
+    })
   }
 })
